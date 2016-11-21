5753b8e75bea7b898d7654b1cd276c4d3fcf5834
Paulus Esterhazy <pesterhazy@gmail.com>
2016-11-21 03:51:16 +0100

    WIP

--- src/tictactoe/root.cljs
+++ src/tictactoe/root.cljs
@@ -26,10 +26,10 @@
 
 (prn @!state)
 
-(defn cell [{:keys [on-press value]}]
+(defn cell [{:keys [on-press value winner?]}]
   [rn/touchable-highlight {:on-press on-press}
    [rn/view {:style cell-style}
-    [rn/text {:style cell-text-style}
+    [rn/text {:style [cell-text-style (when winner? {:color :red})]}
      (case value
        :x "X"
        :o "O"
@@ -37,19 +37,45 @@
 
 (def size 3)
 
-(defn move [x y]
-  (swap! !state (fn [state]
-                  (-> state
-                      (assoc-in [:board x y] (player))
-                      (assoc :player (if (= :o (player)) :x :o))))))
+(def combinations
+  (concat (map #(map vector (repeat %) (range size)) (range size))
+          (map #(map vector (range size) (repeat %)) (range size))
+          [(map #(vector % %) (range size))
+           (map #(vector % (dec (- size %))) (range size))]))
 
-(defn board []
+(defn wins? [board combination]
+  (prn "board" board)
+  (prn "combination:" combination)
+  (let [fields (map #(get-in board %) combination)]
+    (when (and (apply = fields) (first fields))
+      combination)))
+
+(defn winner [board]
+  (some (partial wins? board) combinations))
+
+(defn move [state x y]
+  (if (or (:winner state) (get-in state [:board x y]))
+    state
+    (assoc-in state [:board x y] (:player state :x))))
+
+(defn next-player [state]
+  (assoc state :player (if (= :o (:player state)) :x :o)))
+
+(defn decide [state]
+  (assoc state :winner (winner (:board state))))
+
+(defn board-ui []
   (into [rn/view]
         (map (fn [y]
                (into
                 [rn/view {:style {:flex-direction :row}}]
                 (map (fn [x] [cell {:value (get-in @!state [:board x y])
-                                    :on-press #(move x y)}]) (range size))))
+                                    :winner? (some-> @!state :winner set (get [x y]))
+                                    :on-press (fn []
+                                                (swap! !state #(-> %
+                                                                   (move x y)
+                                                                   next-player
+                                                                   decide)))}]) (range size))))
              (range size))))
 
 (defn root-view
@@ -57,7 +83,7 @@
   [rn/view {:style {:flex 1
                     :justify-content :center
                     :align-items :center}}
-   [board]
+   [board-ui]
    [rn/touchable-highlight {:on-press #(reset! !state nil)}
     [rn/view {:style {:margin-top 10
                       :padding 10
