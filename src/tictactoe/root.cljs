a6262c45d53cd529005df1a2ec2a7ed3e0f7c849
Paulus Esterhazy <pesterhazy@gmail.com>
2016-11-21 02:48:48 +0100

    WIP

--- src/tictactoe/root.cljs
+++ src/tictactoe/root.cljs
@@ -5,12 +5,34 @@
 (def default-font {:font-family "Helvetica"
                    :font-size 20})
 
+(def cell-style
+  {:width 80,
+   :height 80,
+   :borderRadius 5,
+   :backgroundColor "#7b8994",
+   :margin 5,
+   :flex 1,
+   :justifyContent :center,
+   :alignItems :center})
+
 (defonce !state (r/atom nil))
+
+(defn cell []
+  [rn/view {:style cell-style}])
+
+(def size 3)
+
+(defn board []
+  (into [rn/view]
+        (map (fn [y]
+               (into
+                [rn/view {:style {:flex-direction :row}}]
+                (map (fn [x] [cell]) (range size))))
+             (range size))))
 
 (defn root-view
   []
   [rn/view {:style {:flex 1
                     :justify-content :center
                     :align-items :center}}
-   [rn/text {:style default-font}
-    (str "Welcome to boot-react-native")]])
+   [board]])
